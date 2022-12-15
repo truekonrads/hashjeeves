409bb613a78015b955db117de84fb077b331e6dc
Konrads Klints <kklints@kpmg.com.sg>
2022-12-15 03:52:22 +0000

    first concurrently fast version

--- api.py
+++ api.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Header, Query
+from fastapi import FastAPI, Header, Query, HTTPException
 
 app = FastAPI()
 from coredis import Redis
@@ -11,20 +11,20 @@ from datetime import datetime
 import pytz
 import aiohttp
 import ujson as json
-import logging
-import sys
+# import logging
 
 # if sys.platform == 'win32':
 #     loop = asyncio.ProactorEventLoop()
 #     asyncio.set_event_loop(loop)
 # from pydantic import BaseModel
-
+from fastapi.logger import logger
 REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379/0")
 VT_API_KEY = os.getenv("VT_API_KEY", "")
 MAX_VT_REQUESTS = int(os.getenv("MAX_VT_REQUESTS", 100))
 MAX_TOTAL_REQUESTS = int(os.getenv("MAX_VT_REQUESTS", 500))
-LOGGER = logging.getLogger(__name__)
-
+# LOGGER = logging.getLogger(__name__)
+# logger.setLevel(logging.DEBUG)
+# LOGGER.setLevel(logging.DEBUG)
 import itertools
 
 # https://alexwlchan.net/2018/12/iterating-in-fixed-size-chunks/
@@ -57,6 +57,8 @@ def vt_exc_check(e: aiohttp.ClientResponseError):
 
 @backoff.on_exception(backoff.expo, (aiohttp.ClientResponseError), giveup=vt_exc_check)
 async def vtfetch(hash: str, apikey: str):
+    # print(f"vtf: {hash}")
+    # LOGGER.debug(f"VT fetch {hash}")
     async with app.vt_semaphore:
         url = f"https://www.virustotal.com/api/v3/search"
         headers = {"x-apikey": apikey}
@@ -70,6 +72,9 @@ async def vtfetch(hash: str, apikey: str):
                 return j
             except (AttributeError, KeyError, IndexError):
                 return None
+            finally:
+                pass
+                # print(f"vtf-end: {hash}")
 
 
 async def fetch_one(hash, apikey, since, stats_only):
@@ -77,10 +82,12 @@ async def fetch_one(hash, apikey, since, stats_only):
     retval = {"hash": hash}
     redis_cache = await app.redis.hgetall(k)
     if redis_cache:
+        
         retval['cache']=True
         ts = datetime.fromtimestamp(int(redis_cache["ts"]), tz=pytz.UTC)
         retval["ts"] = ts
         if since is None or since <= ts:
+            # print(f"hash:{hash} redis cache hit")
             # print("REDIS")
             j = json.loads(redis_cache["vtdata"])
             # print(j)
@@ -93,6 +100,9 @@ async def fetch_one(hash, apikey, since, stats_only):
             else:
                 retval["vtdata"] = j
             return retval
+        else:
+            pass
+            # print(f"hash:{hash} redis cache hit but expired: ts:{ts} / since:{since}")
             # return {"hash": hash, "ts": rez["ts"], "stats": stats}
 
     # fetch from VT:
@@ -127,11 +137,33 @@ async def fetchresults(
         # Ensure out datetime is timestamp aware
         if since.tzinfo is None:
             since.replace(tzinfo=pytz.UTC)
+    
+    async def call_after(f,after_f,*args,**kwargs):
+        try:
+            return await f(*args,**kwargs)
+        finally:
+            # print(f"after: {after_f}")
+            after_f()
+    
+    
+
+
     results = []
-    for chunk in chunked_iterable(hashes, MAX_TOTAL_REQUESTS):
-        taskbag = [fetch_one(h, apikey, since, stats_only) for h in chunk]
-        results.extend(await asyncio.gather(*taskbag))
+    taskbag=[]
+    for h in hashes:
+        # print(f"hash:{h} waiting to acquire sempahore")
+        await app.task_semaphore.acquire()
+        # partial(asyncio.create_task,)
+        task=asyncio.create_task(call_after(fetch_one,app.task_semaphore.release,h, apikey,since,stats_only))
+        taskbag.append(task)
+        # print(f"hash:{h} put task in a bag")
+    results.extend(await asyncio.gather(*taskbag))
+    # print(results)
     return results
+    # for chunk in chunked_iterable(hashes, MAX_TOTAL_REQUESTS):
+    #     taskbag = [fetch_one(h, apikey, since, stats_only) for h in chunk]
+    #     results.extend(await asyncio.gather(*taskbag))
+    # return results
     # results = await gather_with_concurrency(MAX_TOTAL_REQUESTS,*taskbag)
 
     # for chunk in chunked_iterable(hashes,MAX_TOTAL_REQUESTS):
@@ -149,11 +181,17 @@ async def lookup(
     since: str | None = Query(default=None, description="Not older than timestamp"),
     x_apikey: str = Header(default=VT_API_KEY),
 ):
-    return await fetchresults(hashes, x_apikey, since=since, stats_only=stats_only)
+    try:
+        return await fetchresults(hashes, x_apikey, since=since, stats_only=stats_only)
+    except Exception as e:
+        # print("here")
+        raise HTTPException(500,detail=str(e))
 
 
 @app.on_event("startup")
-def init():
-    print("initialising redis")
+async def init():
+    # print("initialising redis")
     app.vt_semaphore = asyncio.Semaphore(MAX_VT_REQUESTS)
+    app.task_semaphore = asyncio.Semaphore(MAX_TOTAL_REQUESTS)
     app.redis = Redis.from_url(REDIS_URL, decode_responses=True)
+    print(await asyncio.wait_for(app.redis.ping("REDIS READY"),2))
