54fc40b147bcadb785c13e7c2eae9564f0815d61
Konrads Klints <kklints@kpmg.com.sg>
2024-07-09 03:18:35 +0000

    error fix when hash not found

--- api.py
+++ api.py
@@ -1,32 +1,28 @@
+from fastapi.logger import logger
+import itertools
+import uvicorn
+import logging
+import ujson as json
+import aiohttp
+import pytz
+from datetime import datetime
+from dateutil.parser import parse
+import backoff
+from typing import *
+import os
+import asyncio
+from coredis import Redis
 from fastapi import FastAPI, Header, Query, HTTPException
 
 app = FastAPI()
-from coredis import Redis
-import asyncio
-import os
-from typing import *
-import backoff
-from dateutil.parser import parse
-from datetime import datetime
-import pytz
-import aiohttp
-import ujson as json
-import logging
-import uvicorn
 logger = logging.getLogger('uvicorn.debug')
-# if sys.platform == 'win32':
-#     loop = asyncio.ProactorEventLoop()
-#     asyncio.set_event_loop(loop)
-# from pydantic import BaseModel
-from fastapi.logger import logger
+
+
 REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379/0")
 VT_API_KEY = os.getenv("VT_API_KEY", "")
 MAX_VT_REQUESTS = int(os.getenv("MAX_VT_REQUESTS", 100))
 MAX_TOTAL_REQUESTS = int(os.getenv("MAX_VT_REQUESTS", 500))
-# LOGGER = logging.getLogger(__name__)
-# logger.setLevel(logging.DEBUG)
-# LOGGER.setLevel(logging.DEBUG)
-import itertools
+
 
 # https://alexwlchan.net/2018/12/iterating-in-fixed-size-chunks/
 def chunked_iterable(iterable, size):
@@ -57,40 +53,36 @@ def vt_exc_check(e: aiohttp.ClientResponseError):
 
 
 @backoff.on_exception(backoff.expo, (aiohttp.ClientResponseError), giveup=vt_exc_check)
-async def vtfetch(url:str, apikey: str,params:dict):
+async def vtfetch(url: str, apikey: str, params: dict):
     # print(f"vtf: {hash}")
     # LOGGER.debug(f"VT fetch {hash}")
     async with app.vt_semaphore:
         headers = {"x-apikey": apikey}
-        # params = {"query": hash}
-        async with aiohttp.ClientSession(raise_for_status=True) as session:
+        async with aiohttp.ClientSession(raise_for_status=False) as session:
             response = await session.get(url, headers=headers, params=params)
-            # response = await session.get(url, headers=headers)
-
-            j = json.loads(await response.text())
-            try:
-                # stats = j["data"][0]["attributes"]["last_analysis_stats"]
+            # 404 means ismply that hash is not found
+            if response.status in (200, 404):
+                j = json.loads(await response.text())
                 return j
-            except (AttributeError, KeyError, IndexError):
-                return None
-            finally:
-                pass
-                # print(f"vtf-end: {hash}")
+            else:
+                response.raise_for_status()
 
-async def vtfetch_file(hash:str,apikey:str):
+
+async def vtfetch_file(hash: str, apikey: str):
     url = f"https://www.virustotal.com/api/v3/files/{hash}"
-    params={}
-    return await vtfetch(url,apikey,params)
+    params = {}
+    return await vtfetch(url, apikey, params)
 
-async def vtfetch_file_comments(vt_id:str,apikey:str):
+
+async def vtfetch_file_comments(vt_id: str, apikey: str):
     url = f"https://www.virustotal.com/api/v3/files/{vt_id}/comments"
     # 40 is the maximum than can be retrieved in one go
-    params={"limit":40}
-    #TODO: implement cursor to fetch > 40 comments
-    return await vtfetch(url,apikey,params)
+    params = {"limit": 40}
+    # TODO: implement cursor to fetch > 40 comments
+    return await vtfetch(url, apikey, params)
 
 
-async def fetch_redis_cache(key:str,since:int):
+async def fetch_redis_cache(key: str, since: int):
     """
     Retrieves cached data from Redis if available and not expired.
     Args:
@@ -107,30 +99,31 @@ async def fetch_redis_cache(key:str,since:int):
             return redis_cache
     return None
 
-async def fetch_one(hash, apikey:str, since:int, stats_only:bool,comments:bool):
+
+async def fetch_one(hash, apikey: str, since: int, stats_only: bool, comments: bool):
     # Lowercase the hash for consistency
     k = f"hash:{hash.lower()}"
     retval = {"hash": hash}
-    redis_cache = await fetch_redis_cache(k,since)
-    
+    redis_cache = await fetch_redis_cache(k, since)
+
     if redis_cache:
-        
-        retval['cache']=True
+
+        retval['cache'] = True
         ts = datetime.fromtimestamp(int(redis_cache["ts"]), tz=pytz.UTC)
         retval["ts"] = ts
-        vt_data=json.loads(redis_cache["vtdata"])
+        vt_data = json.loads(redis_cache["vtdata"])
     else:
         # fetch from VT:
         ts = int(datetime.now(tz=pytz.UTC).timestamp())
         retval["ts"] = ts
-        retval["cache"]=False
+        retval["cache"] = False
         vt_results = await vtfetch_file(hash, apikey)
         if vt_results:
             await app.redis.hset(k, {"ts": ts, "vtdata": json.dumps(vt_results)})
-            vt_data=vt_results
+            vt_data = vt_results
         else:
-            raise HTTPException(500,"No VT data was returned")
-        
+            raise HTTPException(500, "No VT data was returned")
+
     if stats_only:
         try:
             stats = vt_data["data"]["attributes"]["last_analysis_stats"]
@@ -140,30 +133,28 @@ async def fetch_one(hash, apikey:str, since:int, stats_only:bool,comments:bool):
             pass
     else:
         retval["vtdata"] = vt_data
-        
-    if comments:
-        vt_id=vt_data['data']['id']
-        comment_key=f"comment:{vt_id}"
-        redis_cache = await fetch_redis_cache(comment_key,since)
-        if redis_cache:        
-            retval['comment_cache']=True
+    # if 'error' in vt_data:
+    if comments and not ('error' in vt_data):
+        vt_id = vt_data['data']['id']
+        comment_key = f"comment:{vt_id}"
+        redis_cache = await fetch_redis_cache(comment_key, since)
+        if redis_cache:
+            retval['comment_cache'] = True
             ts = datetime.fromtimestamp(int(redis_cache["ts"]), tz=pytz.UTC)
             retval["comment_ts"] = ts
-            comment_data=json.loads(redis_cache["vtdata"])
+            comment_data = json.loads(redis_cache["vtdata"])
         else:
             # fetch from VT:
             ts = int(datetime.now(tz=pytz.UTC).timestamp())
             retval["ts"] = ts
-            retval["comment_cache"]=False            
+            retval["comment_cache"] = False
             comment_data = await vtfetch_file_comments(vt_id, apikey)
             if comment_data:
                 await app.redis.hset(k, {"ts": ts, "comment": json.dumps(comment_data)})
             else:
-                raise HTTPException(500,"No VT comment data was returned")
-        retval['comment_data']=comment_data
+                raise HTTPException(500, "No VT comment data was returned")
+        retval['comment_data'] = comment_data
 
-
-        # rez = {"hash": hash, "ts": ts, "stats": stats}
     return retval
 
 
@@ -179,39 +170,23 @@ async def fetchresults(
         # Ensure out datetime is timestamp aware
         if since.tzinfo is None:
             since.replace(tzinfo=pytz.UTC)
-    
-    async def call_after(f,after_f,*args,**kwargs):
-        try:
-            return await f(*args,**kwargs)
-        finally:
-            # print(f"after: {after_f}")
-            after_f()
-    
-    
 
+    async def call_after(f, after_f, *args, **kwargs):
+        try:
+            return await f(*args, **kwargs)
+        finally:
+            after_f()
 
     results = []
-    taskbag=[]
+    taskbag = []
     for h in hashes:
-        # print(f"hash:{h} waiting to acquire sempahore")
         await app.task_semaphore.acquire()
-        # partial(asyncio.create_task,)
-        task=asyncio.create_task(call_after(fetch_one,app.task_semaphore.release,h, apikey,since,stats_only,comments))
+        task = asyncio.create_task(call_after(
+            fetch_one, app.task_semaphore.release, h, apikey, since, stats_only, comments))
         taskbag.append(task)
-        # print(f"hash:{h} put task in a bag")
     results.extend(await asyncio.gather(*taskbag))
-    # print(results)
+
     return results
-    # for chunk in chunked_iterable(hashes, MAX_TOTAL_REQUESTS):
-    #     taskbag = [fetch_one(h, apikey, since, stats_only) for h in chunk]
-    #     results.extend(await asyncio.gather(*taskbag))
-    # return results
-    # results = await gather_with_concurrency(MAX_TOTAL_REQUESTS,*taskbag)
-
-    # for chunk in chunked_iterable(hashes,MAX_TOTAL_REQUESTS):
-    #     for h in chunk:
-
-    # return results
 
 
 @app.post("/api/lookup")
@@ -223,16 +198,17 @@ async def lookup(
     comments: bool = Query(
         default=False, description="Fetch and return comments"
     ),
-    since: str | None = Query(default=None, description="Not older than timestamp"),
+    since: str | None = Query(
+        default=None, description="Not older than timestamp"),
     x_apikey: str = Header(default=VT_API_KEY),
 ):
     try:
-        if x_apikey is None or x_apikey=="":
+        if x_apikey is None or x_apikey == "":
             logger.error(f"x-apikey is invalid: {x_apikey}")
-        return await fetchresults(hashes, x_apikey, since=since, stats_only=stats_only,comments=comments)
+        return await fetchresults(hashes, x_apikey, since=since, stats_only=stats_only, comments=comments)
     except Exception as e:
         logger.error(f"Error while looking up hashes: {e}")
-        raise HTTPException(500,detail=str(e))
+        raise HTTPException(500, detail=str(e))
 
 
 @app.on_event("startup")
@@ -241,9 +217,9 @@ async def init():
     app.vt_semaphore = asyncio.Semaphore(MAX_VT_REQUESTS)
     app.task_semaphore = asyncio.Semaphore(MAX_TOTAL_REQUESTS)
     app.redis = Redis.from_url(REDIS_URL, decode_responses=True)
-    logger.info(await asyncio.wait_for(app.redis.ping("REDIS READY"),2))
+    logger.info(await asyncio.wait_for(app.redis.ping("REDIS READY"), 2))
 
 if __name__ == "__main__":
-    
+
     logger = logging.getLogger('uvicorn.debug')
-    uvicorn.run(app, host="0.0.0.0", port=8000,log_level="debug")
+    uvicorn.run(app, host="0.0.0.0", port=8000, log_level="debug")
